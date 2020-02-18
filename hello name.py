072426b372b5742042aa0518e54c9457aadf65ff
Elliot <eme5220@stacmail.net>
2020-02-18 15:17:55 +1300

    added task 4

--- hello name.py
+++ hello name.py
@@ -1,7 +1,7 @@
 
+x = int(input("how many names do you require to entry"))
 
-
-for name in range (0,4) :
+for name in range (0, x) :
     name = input("what's your name: ")
     
     print("hello", name ," hows it going ")
