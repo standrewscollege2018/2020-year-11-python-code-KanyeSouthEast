d2662f4303341e36509a0a78574b7c1062904707
Elliot <eme5220@stacmail.net>
2020-03-06 09:49:54 +1300

    ...

---
+++ favourite movies.py
@@ -0,0 +1,7 @@
+"""favourite movies"""
+favourite_movies = []
+for i in range(3):
+    movies= input("whats your favourite movie:")
+    favourite_movies.append(movies)
+    
+print(favourite_movies)
--- speeders task.py
+++ speeders task.py
@@ -1,5 +1,34 @@
 """speedy gonzalis """
+FINE10 = 10
+FINE20 = 20
+FINE30 = 30
 
-name = int(input("what the name there buddy:"))
-limit = int(input("what the speed limit:"))    
-speed = int(input("whats the speed they at:"))
+arrest = ["ryan lee","jake stokes","ethan adams","ben mckowan"]
+
+keep_asking = True
+while keep_asking ==True:
+    try:
+        
+        name = (input("what the name there buddy:"))
+        limit = int(input("what the speed limit:"))    
+        speed = int(input("whats the speed they at:"))    
+    except:
+        print("that the wrong number boi")
+    fast = (speed - limit)  
+    if (name in arrest):
+        print ("Kill",name,"on site")
+        
+    if fast >= FINE10:
+        print("fine this duuude $80")
+            
+    elif fast >= FINE20 :
+        print("this man costs 130 dollar bills")
+        
+    elif fast >FINE30:
+        print("this man on the hoon charge him $180")
+            
+    elif fast < FINE10:
+        print ("Fine",name,"$30 now and let go")    
+       
+        
+        
